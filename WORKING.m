f17a68d0c0d9c275d84b81ec99a40d06a8ad2965
Christian Witting <witting.chr@gmail.com>
2015-09-24 15:36:32 +1000

    Show

--- WORKING.m
+++ WORKING.m
@@ -1,11 +1,10 @@
 delete(imaqfind)
-calibrate = 0;
 detect_pen = 1;
 
 %%
-f = 1056.88172825152;
-cx = 954.508643076111;
-cy = 553.024914356277;
+f = cameraParams.FocalLength(1);
+cx = cameraParams.PrincipalPoint(1);
+cy = cameraParams.PrincipalPoint(2);
 r = cameraParams.RotationMatrices(:,:,end);
 t = cameraParams.TranslationVectors(end,:);
 rot = [[0 1 0];[1 0 0];[0 0 -1]];
@@ -23,44 +22,24 @@ triggerconfig(vidDepth, 'manual');
 start(vidRGB);
 start(vidDepth);
 
-%%Color calibration
-if (calibrate)
-    trigger(vidRGB);
-    col = getdata(vidRGB);
-    imwrite(col,'col.png');
-    T = estimate_color_matrix('col.png', 'GretagMacbeth', 4);
-end
-
 trigger(vidRGB);
 trigger(vidDepth);
 fprintf('Position of Camera: ');
 disp((rot*r*(0-t)' + offset' + framePos' + [0 100 0]')');
 %%
-
-%avg = gpuArray((getdata(vidRGB)));
 avg = getdata(vidRGB);
-%avg = ti_hom_lin( T, avg );
 avg = undistortImage(avg,cameraParams);
-    avg = imcrop(avg,[284 0 512*3 1080]);
-    avg = imresize(avg,1/3);
-    avg = imresize(avg, [361 512]);
-    avg = flip(avg,2);
-%
+avg = imcrop(avg,[284 0 512*3 1080]);
+avg = imresize(avg,1/3);
+avg = imresize(avg, [361 512]);
+avg = flip(avg,2);
 avg = int32(rgb2gray(avg));
-%avg = rgb2hsv(avg);
-%%
-%avg_depth = getdata(vidDepth);
-%for i = 1:10
-%    img = uint16(rgb2gray(snapshot(cam)));
-%    avg = avg + img;
-%end
-%avg = uint8(avg/10);
 se = strel('disk',3);
 while(1)
     trigger(vidRGB);
     trigger(vidDepth);
     RGB = getdata(vidRGB);
-    %RGB = ti_hom_lin( T, RGB);
+
     RGB = undistortImage(RGB,cameraParams);
     depth = getdata(vidDepth);
     depth = imcrop(depth,[0 77/3 512 1080/3]);
@@ -69,47 +48,29 @@ while(1)
     imgRaw = imresize(RGB, [361 512]);
     imgRaw = flip(imgRaw,2);
     depth = flip(depth,2);
-    %imgRaw = gpuArray((getdata(vidRGB)));
-    %depth = getdata(vidDepth);
+
     img = rgb2hsv(imgRaw);
     newBack = int32(rgb2gray(imgRaw));
     imgBin = img(:,:,1) < 50/360 & img(:,:,1) > 0/360;
-%     avg = 0.98* avg + 0.02*newBack;
 
     back = abs((avg)-(newBack)) > 10;
-    %back = abs(avg(:,:,1) - img(:,:,1)) > 0.1;
+
     res = imgBin & back;
     res = imerode(res,se);
-    %res = imdilate(res,se);
-    %CH = bwconvhull(hand,'objects');
+
     st = regionprops(res, 'ConvexHull','Area','Centroid','Solidity','Extrema', 'BoundingBox', 'Eccentricity');
     se = strel('disk',1);
-    %back = imerode(back,se);
+
     
     
-    %res = uint16(res);
-    %imshow(depth*20)
-                penImage = xor(back, res);
-        figure(5)
-        imshow(penImage);
-        title('Pen image')
+
+    penImage = xor(back, res);
     figure(1)
     imshow(res);
     title('Final filter')
-    %imshow(depth*30);
-    %figure(2)
-    %imshow(back);
-    %title('Background subtraction')
-    %title('Background');
-    %figure(3)
-    %imshow(imgBin);
-    %title('Color filter')
-    %title('Color filter');
-    %imshow(img);
     figure(4)
     imshow(imgRaw);
     title('Raw image')
-    %title('Combined');
 
     hold on
     hands = 0;
@@ -137,15 +98,12 @@ while(1)
                 hands = hands +1;
                 fakePos = [xm, ym, zm];
                 realPos = r*(fakePos-t)';
-                % The offset values should be recalculted after new setup!
-                realPos = rot*realPos + [-20 -150 -15]' ;
-                %fprintf('Position: %f, %f, %f)\n', xm,ym,zm);
+                realPos = rot*realPos + offset' ;
                 fprintf('Position pen(%d): %f, %f, %f)\n', pens, realPos(1),realPos(2),realPos(3));
             end
         end
     end
     for k = 1 : length(st);
-        %thisBB = st(k).ConvexHull;
         if (st(k).Solidity < 0.8 && st(k).Area > 1000)
             i= i+1;
             XY = st(k).Centroid;
@@ -158,9 +116,7 @@ while(1)
             hands = hands +1;
             fakePos = [xm, ym, zm];
             realPos = r*(fakePos-t)';
-            % The offset values should be recalculted after new setup!
             realPos = rot*realPos + offset'+ framePos' ;
-            %fprintf('Position: %f, %f, %f)\n', xm,ym,zm);
             fprintf('Position hand(%d): %f, %f, %f)\n', i, realPos(1),realPos(2),realPos(3));
         end
     end
